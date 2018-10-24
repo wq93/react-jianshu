a12a35664ce1f32d95c26774b69b75e3130647b6
wq93 <214180117@qq.com>
2018-10-24 18:34:25 +0800

    完成'热门搜索' 获取数据节流

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -25,7 +25,7 @@ class Header extends Component {
   }
 
   render() {
-    const {focused, handleInputFocus, handleInputBlur} = this.props;
+    const {focused, handleInputFocus, handleInputBlur, list} = this.props;
     return (
       <HeaderWrapper>
         <Logo/>
@@ -44,7 +44,7 @@ class Header extends Component {
             >
               <NavSearch
                 className={focused ? 'focused' : ''}
-                onFocus={() => handleInputFocus()}
+                onFocus={() => handleInputFocus(list)}
                 onBlur={() => handleInputBlur()}>
               </NavSearch>
             </CSSTransition>
@@ -117,9 +117,9 @@ const mapStateToProps = (state) => {
 
 const mapDispathToProps = (dispatch) => {
   return {
-    handleInputFocus() {
+    handleInputFocus(list) {
+      list.size === 0 && dispatch(actionCreators.getList())
       dispatch(actionCreators.searchFocus())
-      dispatch(actionCreators.getList())
     },
     handleInputBlur() {
       dispatch(actionCreators.searchBlur())
